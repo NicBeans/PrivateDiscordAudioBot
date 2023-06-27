1fd053de69222b899338c812e799b6f46c6e8dfb
@NicBeans <nicholasbosman007@gmail.com>
2023-06-27 20:44:22 +0200

    weird commit bongs on join, cant leave. but it can join and slash commands work

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Discord } = require('discord.js');
-const cron = require('node-cron');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits, Collection, Events  } = require('discord.js');
 const { createAudioPlayer, NoSubscriberBehavior, AudioPlayerStatus, createAudioResource, joinVoiceChannel  } = require('@discordjs/voice');
+const { connect } = require('node:http2');
 
-
-const { TOKEN, VOICE_CHANNEL_ID, GUILD_ID, TEXT_CHANNEL_ID, VOICE_CHANNEL_ID_WORK } = process.env;
+const { TOKEN, VOICE_CHANNEL_ID, GUILD_ID, TEXT_CHANNEL_ID, VOICE_CHANNEL_ID_WORK, CLIENT_ID } = process.env;
 
 const client = new Client({ intents: [
 	GatewayIntentBits.Guilds,
@@ -18,7 +19,27 @@ const client = new Client({ intents: [
 	GatewayIntentBits.MessageContent,
   ]});
 
-let guild, voiceChannel, voiceChannelWork, textChannel ;
+  client.commands = new Collection();
+
+  const foldersPath = path.join(__dirname, 'commands');
+  const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+let guild, voiceChannel, voiceChannelWork, textChannel, clientID, connection;
 
 // When bot comes online check the guild and voice channel are valid
 // if they are not found the program will exit
@@ -32,106 +53,65 @@ client.on('ready', async () => {
 		process.exit(1);
 	}
 	textChannel = guild.channels.cache.get(TEXT_CHANNEL_ID);
-	console.log('Big Ben Ready...');
+	console.log('Music ready...');
 	//console.log(guild, voiceChannel, voiceChannelWork, textChannel)
-	client.user.setPresence({ activities: [{ name: 'the waiting game...', }], status: 'idle' });
+	client.user.setPresence({ activities: [{ name: 'jammin...', }], status: 'idle' });
 });
 
-// use node-cron to create a job to run every hour
-const task = cron.schedule('0 0 */1 * * *', async () => {
-	let { hour, amPm, timezoneOffsetString } = getTimeInfo();
-	// console.log('1min');
-	// if text channel was defined send message in chat
-	// if (textChannel) {
-	// 	textChannel.send(`The time is now ${hour}:00 ${amPm} GMT${timezoneOffsetString}`);
-	// }
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-	// check if VC defined in config is empty
-	if (voiceChannel.members.size >= 1) {
-		try {
-			client.user.setPresence({ activities: [{ name: 'BONG', }], status: 'online' });
-			// connect to voice channel
+	const command = interaction.client.commands.get(interaction.commandName);
+	console.log(command);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	else if (command.data.name === 'stop') {
+		if (!voiceChannel) { 
+			await interaction.reply({ content: 'I am not in a voice channel!', ephemeral: true });
+		}
+		
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (interaction.isChatInputCommand()) {
+		if (interaction.commandName === 'play') {
+			const voiceChannel = interaction.options.getChannel('channel');
+		if (voiceChannel.members.size >= 1) {
+			try {
+			const resource = createAudioResource('bigben.mp3');
+			const player = createAudioPlayer()
 			const connection = await joinVoiceChannel({
 				channelId: voiceChannel.id,
 				guildId: guild.id,
-				adapterCreator: guild.voiceAdapterCreator,
+				adapterCreator: interaction.guild.voiceAdapterCreator
 			});
-			// counter for looping
-			let count = 1;
-		
-			// immediately invoked function that loops to play the bell sound 
-			(function play() {
-				console.log('playing');
-				const resource = createAudioResource('bigben.mp3');
-				const player = createAudioPlayer();
-				player.play(resource);
-				connection.subscribe(player);
-				
-				player.on(AudioPlayerStatus.Idle, () => {
-					connection.destroy();
-					client.user.setPresence({ activities: [{ name: 'the waiting game...', }], status: 'idle' });
-				})
-			})();
+			player.play(resource);	
+			connection.subscribe(player);
+			// console.log(voiceChannel);
+			// console.log(guild);
 		} catch(error) {
 			console.log(error);
 		}
-	}
-// });
-
-	if (voiceChannelWork.members.size >= 1) {
-		try {
-			client.user.setPresence({ activities: [{ name: 'BONG', }], status: 'online' });
-			// connect to voice channel
-			const connection = await joinVoiceChannel({
-				channelId: voiceChannelWork.id,
-				guildId: guild.id,
-				adapterCreator: guild.voiceAdapterCreator,
-			});
-			// counter for looping
-			let count = 1;
 		
-			// immediately invoked function that loops to play the bell sound 
-			(function play() {
-				console.log('playing');
-				const resource = createAudioResource('bigben.mp3');
-				const player = createAudioPlayer();
-				player.play(resource);
-				connection.subscribe(player);
-				
-				player.on(AudioPlayerStatus.Idle, () => {
-					connection.destroy();
-					client.user.setPresence({ activities: [{ name: 'the waiting game...', }], status: 'idle' });
-				})
-			})();
-
-		} catch(error) {
-			console.log(error);
-		}
 	}
-});
-// function to get current time and return object containing
-// hour and if it is am or pm
-const getTimeInfo = () => {
-	    
-		let time = new Date();
-		// let minutes = time.getMinutes();
-		let hour = time.getHours() >= 12 ? time.getHours() - 12 : time.getHours();
-		hour = hour === 0 ? 12 : hour;
-		let amPm = time.getHours() >= 12 ? 'PM' : 'AM';
-		// get gmt offset in minutes and convert to hours
-		let gmtOffset = time.getTimezoneOffset() / 60
-		// turn gmt offset into a string representing the timezone in its + or - gmt offset
-		let timezoneOffsetString = `${gmtOffset > 0 ? '-':'+'} ${Math.abs(gmtOffset)}`;
+}}});
+			
 
-	return {
-		// minutes,
-		hour,
-		amPm,
-		timezoneOffsetString
-	}
-}
-
-// start the cron job
-task.start();
 
 client.login(TOKEN);
---
+++ commands/music/playsong.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('plays a song')
+        .addChannelOption(option => option.setName('channel').setDescription('The voice channel to join').setRequired(true).addChannelTypes(ChannelType.GuildVoice)),
+	async execute(interaction) {
+		// await interaction.reply('Pong!');
+	},
+};
